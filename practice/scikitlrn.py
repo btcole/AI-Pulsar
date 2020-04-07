b7f360d7650b5bbe1414bdb66cec66e9df6915f1
Trey Cole <treco713@gmail.com>
2020-04-07 17:54:39 -0400

    predictions

--- practice/scikitlrn.py
+++ practice/scikitlrn.py
@@ -1,13 +1,27 @@
-
-import platform
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 os = platform.system()
 if os.lower() == "windows":
     data = pd.read_csv("..\data\pulsar_stars.csv")
 else:
     data = pd.read_csv("../data/pulsar_stars.csv")
-print(data.columns)
-print(data)
+
+clf = RandomForestClassifier(random_state=0)
+X = data[:, 0:8]
+Y = data[:, 8]
+print(data.shape, X.shape, Y.shape)
+
+clf.fit(X, Y)
+prediction = clf.predict(X)
+
+print(all(Y == prediction))
+
+false_count = 0
+for i in range(Y.shape[0]):
+    if prediction[i] == Y[i]:
+        false_count += 1
+
+print((false_count/Y.shape[0])*100)
