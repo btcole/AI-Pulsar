8f2ca320efbe7fb3961982e894fa540cdc3db66d
Trey Cole <treco713@gmail.com>
2020-04-24 09:47:33 -0400

    chngs

--- practice/sklearn/scikitlrn.py
+++ practice/sklearn/scikitlrn.py
@@ -2,8 +2,9 @@ import platform
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split #splitting data into training and testing subsets
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 
@@ -13,23 +14,30 @@ if os.lower() == "windows":
 else:
     data = pd.read_csv("../../data/pulsar_stars.csv")
 
-clf = RandomForestClassifier(random_state=10)
-data = np.array(data)
+data.head() #overview of dataset, not scaled well
+
+data = np.array(data) #changed to numpy array so we can split the array
 X = data[:, 0:8]
 Y = data[:, 8]
 print(data.shape, X.shape, Y.shape)
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+clf = RandomForestClassifier(random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+"""
+test_size is 0=>1 representing percentage of dataset used for testing and percentage used for training
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=10)
-clf.fit(X_train, Y_train)
-prediction = clf.predict(X_test)
+"""
+sc = StandardScaler() #scaling the features
+x_train = sc.fit_transform(x_train) #
+x_test = sc.transform(x_test)
 
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+clf.fit(x_train, y_train)
+prediction = clf.predict(x_test)
+
+print('Accuracy:', metrics.accuracy_score(y_test, prediction))
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, prediction))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, prediction))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, prediction)))
 
 #print(all(Y == prediction))
 
